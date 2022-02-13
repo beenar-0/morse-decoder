283b1b4e120175f9e4f042bd6fea31f830c64138
beenar-0 <dncastledn@gmail.com>
2022-02-14 02:40:45 +0300

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    expr = expr.split('')
+    let newTable = {}
+    let newKeys = []
+    let temp = []
+    let symCount = expr.length/10
+    let keys = Object.keys(MORSE_TABLE)
+    let letters = Object.values(MORSE_TABLE)
+    keys = keys.map((item)=>{
+        for (let i = 0; i < item.length;i++) {
+            if (item[i] === '.') temp.push(10)
+            else if (item[i] === '-') temp.push(11)
+        }
+        newKeys.push(temp.join('').padStart(10,'0'))
+        temp = []
+    })
+    for (let i = 0; i < keys.length; i++) {
+        newTable[newKeys[i]] = letters[i]
+    }
+    for (let i = 0; i < symCount; i++){
+        temp.push(expr.splice(0,10).join(''))
+    }
+    expr = Object.values(temp)
+    temp = []
+    expr.forEach((item)=>{
+        if (item === '**********') temp.push(' ')
+        else  temp.push(newTable[item])
+    })
+    return temp.join('')
 }
 
 module.exports = {
